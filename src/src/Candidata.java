d43d729ed04fe420fe5c91405d9dc67800095a8a
josue <josue.patino@epn.edu.ec>
2025-11-21 12:07:31 -0500

    Clase abstracta creada

---
+++ src/src/Candidata.java
@@ -0,0 +1,58 @@
+public abstract class Candidata {
+    protected int id;
+    protected  String nombre;
+    protected int edad;
+    protected  String distrito;
+    protected double puntajeJurado;
+
+    public Candidata(int id, String nombre, int edad, String distrito) {
+         this.id = id;
+         this.nombre = nombre;
+         this.edad = edad;
+         this.distrito = distrito;
+         this.puntajeJurado = 0;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public double getPuntajeJurado() {
+        return puntajeJurado;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public void setPuntajeJurado(double puntajeJurado) {
+        this.puntajeJurado = puntajeJurado;
+    }
+
+    public abstract void mostrarDetalles();
+}
